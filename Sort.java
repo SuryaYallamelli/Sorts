1da4fff85144cd457e28790a71ec76adaa7b87f5
surya <suryanarayana4870@gmail.com>
2022-04-25 21:30:39 +0530

    first commit

---
+++ Sort.java
@@ -0,0 +1,32 @@
+// program to implement Bubble Sort
+
+class Sort{
+
+  public static void printBubble(int arr[]){
+
+    for(int i = 0; i < arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+  public static void main(String[] args){
+    int arr[] = {7,9,6,3,1,5};
+
+    // Bubble sort
+    for(int i = 0; i < arr.length-1; i++){
+      for(int j = 0; j < arr.length-i-1; j++){
+        if(arr[j] > arr[j+1]){
+
+          // swap
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+
+        }
+      }
+    }
+
+    printBubble(arr);
+
+  }
+}
